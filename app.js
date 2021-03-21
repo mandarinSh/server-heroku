178e312bc06562444c0b8da6472d9336b256ebc3
Alina Pereskokova <Alina.Pereskokova@emc.com>
2021-03-21 23:52:32 +0300

    add Procfile

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-// const express = require('express');
-// const app = express();
-
-// app.get('/', (request, response) => {
-//   response.send('Hello from server!');
-// });
-
-// // app.get('/getDogs', (request, response) => {
-// //   let dogs = {
-// //     dogNames: ['Zeus', 'Kolt', 'Kiara']
-// //   };
-// //   response.send(dogs);
-// // });
-
-// app.use((err, request, response, next) => {
-//   // логирование ошибки, пока просто console.log
-//   console.log(err);
-//   response.status(500).send('Something broke!');
-// });
-
-// app.listen(3000);
-
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
