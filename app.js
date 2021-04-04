bcbfdd1461ca3f4398cc298971db53d46f5dfc0a
Alina Pereskokova <Alina.Pereskokova@emc.com>
2021-04-05 02:47:18 +0300

    add cors handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,6 +21,21 @@ app.get('/getDogs', (request, response) => {
     }
   ];
   response.send(dogs);
+});
+
+app.get('/getDogs1', (request, response, next) => {
+  let dogs = [
+    {
+      id: 1,
+      name: "Zeus"
+    },
+    {
+      id: 2,
+      name: "Kiara"
+    }
+  ];
+  // response.send(dogs);
+  response.json(dogs);
 });
 
 app.get('/getCats', (request, response) => {
